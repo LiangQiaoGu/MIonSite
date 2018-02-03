99f786056ac4d7cf5b45c2ecc0eb6bd19bf78bf7
LiangQiaoGu <33341161+LiangQiaoGu@users.noreply.github.com>
2018-02-03 20:14:19 +0800

    Add files via upload

---
+++ standardalone_MIonSite/setup.sh
@@ -0,0 +1,62 @@
+#!/bin/tcsh
+
+tar zxvf ./java.tar.gz
+
+echo "Decompress the model files\n"
+
+tar zxvf ./models/ZN/svm_1.tar.gz
+tar zxvf ./models/ZN/svm_2.tar.gz
+tar zxvf ./models/ZN/svm_3.tar.gz
+tar zxvf ./models/ZN/svm_4.tar.gz
+tar zxvf ./models/ZN/svm_5.tar.gz
+tar zxvf ./models/ZN/svm_6.tar.gz
+tar zxvf ./models/ZN/svm_7.tar.gz
+tar zxvf ./models/ZN/svm_8.tar.gz
+tar zxvf ./models/ZN/svm_9.tar.gz
+tar zxvf ./models/ZN/svm_10.tar.gz
+
+tar zxvf ./models/CA/svm_1.tar.gz
+tar zxvf ./models/CA/svm_2.tar.gz
+tar zxvf ./models/CA/svm_3.tar.gz
+tar zxvf ./models/CA/svm_4.tar.gz
+tar zxvf ./models/CA/svm_5.tar.gz
+tar zxvf ./models/CA/svm_6.tar.gz
+tar zxvf ./models/CA/svm_7.tar.gz
+tar zxvf ./models/CA/svm_8.tar.gz
+tar zxvf ./models/CA/svm_9.tar.gz
+tar zxvf ./models/CA/svm_10.tar.gz
+
+tar zxvf ./models/MG/svm_1.tar.gz
+tar zxvf ./models/MG/svm_2.tar.gz
+tar zxvf ./models/MG/svm_3.tar.gz
+tar zxvf ./models/MG/svm_4.tar.gz
+tar zxvf ./models/MG/svm_5.tar.gz
+tar zxvf ./models/MG/svm_6.tar.gz
+tar zxvf ./models/MG/svm_7.tar.gz
+tar zxvf ./models/MG/svm_8.tar.gz
+tar zxvf ./models/MG/svm_9.tar.gz
+tar zxvf ./models/MG/svm_10.tar.gz
+tar zxvf ./models/MG/svm_11.tar.gz
+
+tar zxvf ./models/MN/svm_1.tar.gz
+tar zxvf ./models/MN/svm_2.tar.gz
+tar zxvf ./models/MN/svm_3.tar.gz
+tar zxvf ./models/MN/svm_4.tar.gz
+tar zxvf ./models/MN/svm_5.tar.gz
+tar zxvf ./models/MN/svm_6.tar.gz
+tar zxvf ./models/MN/svm_7.tar.gz
+tar zxvf ./models/MN/svm_8.tar.gz
+tar zxvf ./models/MN/svm_9.tar.gz
+tar zxvf ./models/MN/svm_10.tar.gz
+
+tar zxvf ./models/FE/svm_1.tar.gz
+tar zxvf ./models/FE/svm_2.tar.gz
+tar zxvf ./models/FE/svm_3.tar.gz
+tar zxvf ./models/FE/svm_4.tar.gz
+tar zxvf ./models/FE/svm_5.tar.gz
+tar zxvf ./models/FE/svm_6.tar.gz
+tar zxvf ./models/FE/svm_7.tar.gz
+tar zxvf ./models/FE/svm_8.tar.gz
+tar zxvf ./models/FE/svm_9.tar.gz
+
+echo "Have a good day"
